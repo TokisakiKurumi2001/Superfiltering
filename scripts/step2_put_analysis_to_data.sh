3c485f5235a77ba8dc3fd9135b337170dbe20ad2
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2024-06-29 16:48:30 +0700

    Correct data path

--- scripts/step2_put_analysis_to_data.sh
+++ scripts/step2_put_analysis_to_data.sh
@@ -1,6 +1,6 @@
 
 python code_ifd/put_analysis_to_data.py \
-    --pt_data_path ppl_score.jsonl \
+    --pt_data_path data_dir/ppl_score.jsonl \
     --json_data_path data_dir/syn.jsonl \
     --json_save_path data_dir/syn_with_ifd_score.jsonl
 
